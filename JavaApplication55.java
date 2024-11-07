8218d026e5bea0037d9ba196261c6f522312a2a1
gerasantillan17 <gerapumas122@gmail.com>
2024-11-07 00:08:03 -0600

    Add files via upload

---
+++ JavaApplication55.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication55;
+
+/**
+ *
+ * @author gerar
+ */
+public class JavaApplication55 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int dato, valores[];
+       int[] datos, x, y, z;
+       int[] arreglo = new int[100];
+       for(int i=0; i < arreglo.length; i++){
+           arreglo[1]=(int)(Math.random()*100);
+           
+       }
+       for(int i=0; i < arreglo.length; i++){
+       System.out.println(arreglo[i]+"-");
+       
+    }
+    
+    }
+}
